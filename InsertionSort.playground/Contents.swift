afbac43e64295c955a61fe675a6f6d7f39e81153
huda_elhady1 <huda.elhaty@raye7.com>
2020-06-19 16:25:10 +0200

    making insertion sort generic

--- InsertionSort.playground/Contents.swift
+++ InsertionSort.playground/Contents.swift
@@ -1,18 +1,37 @@
 import Foundation
 
-func insertionSort( array: inout [Int]) -> [Int] {
-    
-    for i in 1 ..< array.count {
+func insertionSort( array:[Int]) -> [Int] {
+    var a = [Int]()
+    for i in 0 ..< array.count {
+        print(i)
         var y = i
-        while y > 0 , array[y] < array[y-1] {
-            let temp = array[y-1]
-            array[y-1] = array[y]
-            array[y] = temp
+        a.append(array[i])
+        while y > 0 , a[y] < a[y-1] {
+            let temp = a[y-1]
+            a[y-1] = a[y]
+            a[y] = temp
             y -= 1
         }
     }
-    return array
+    return a
+}
+
+func insertionInPlaceSort<T>( array: [T], isOrderedBefore: (T,T) -> Bool) -> [T] {
+    var a = array
+    for i in 1 ..< a.count {
+        var y = i
+        let temp = a[y]
+        while y > 0 , isOrderedBefore(temp , a[y-1]) {
+            a[y] = a[y-1]
+            y -= 1
+        }
+        a[y] = temp
+    }
+    return a
 }
 
 var a = [ 8, 3, 5, 4, 6 ]
-print(insertionSort(array: &a))
+print(insertionInPlaceSort(array: a, isOrderedBefore: <))
+let strings = [ "b", "a", "d", "c", "e" ]
+print(insertionInPlaceSort(array: strings, isOrderedBefore: <))
+//print(insertionSort(array: a))
